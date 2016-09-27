96d40ea1e8225d5498118d467decb4765621af5f
Adria Rodriguez <adria_r_p_@hotmail.com>
2016-09-27 19:53:24 +0200

    add build gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,14 @@ var del = require('del');
 var gulpSequence = require('gulp-sequence');
 var sourcemaps = require('gulp-sourcemaps');
 var runElectron = require('gulp-run-electron');
+var packager = require('electron-packager');
+var util = require('gulp-util');
+var slash = require('slash');
+
 var paths = {
     src: "src",
-    dist: "dist"
+    dist: "dist",
+    release: "release"
 };
 
 gulp.task('ts', function () {
@@ -28,19 +33,78 @@ gulp.task('clean', function () {
 });
 
 
-gulp.task('build', gulpSequence('clean',['templates', 'ts']));
+gulp.task('clean:release', function () {
+    del.sync([paths.release]);
+});
+
+gulp.task('build', gulpSequence('clean', ['templates', 'ts']));
 
 gulp.task('app:live-reload', ['build'], function () {
-    gulp.watch(paths.src + '/**/*.ts', ['ts',runElectron.rerun]);
-    gulp.watch(paths.src + '/**/*.html', ['templates',runElectron.rerun]);
-    return createElectronApp();
+    gulp.watch(paths.src + '/**/*.ts', ['ts', runElectron.rerun]);
+    gulp.watch(paths.src + '/**/*.html', ['templates', runElectron.rerun]);
+    return runElectronApp();
 });
 
 gulp.task('app:run', ['build'], function () {
-    return createElectronApp();
+    return runElectronApp();
 });
 
-function createElectronApp(){
+gulp.task('app:build', ['build'], function (done) {
+    var packageJson = require('./package.json');
+
+    var ignoreDevDep = [
+        /src/,
+        /node_modules\/gulp/
+    ];
+
+    var options = {
+        'dir': __dirname,
+        'name': packageJson.name,
+        'app-version': packageJson.version,
+        'build-version': packageJson.devDependencies["electron-prebuilt"],
+        'out': __dirname + '/' + paths.release,
+        'ignore': ignoreDevDep,
+        arch:"all",
+        tmpdir: false,
+        overwrite: true,
+        'afterCopy': [function (buildPath, electronVersion, platform, arch, callback) {
+            util.log(util.colors.cyan("delete from"), buildPath);
+            var path = slash(buildPath);
+
+            del.sync([path + '/**',
+                path + '/.*',
+                '!' + path,
+                '!' + path + "/node_modules/**",
+                '!' + path + "/dist/**",
+                '!' + path + "/package.json"
+            ], {force: true});
+
+            gulp.src(path + "/dist/**/*.*")
+                .pipe(gulp.dest(path))
+                .on('end', function() {
+                    del.sync(path + '/dist/**', {force: true});
+                    callback();
+                });
+
+        }]
+    };
+
+    packager(options, function (err, appPath) {
+        if (err) {
+            util.log(err)
+        }
+        else {
+            util.log('Built', util.colors.cyan(options.name), util.colors.magenta('v' + options['app-version']));
+            util.log('Packaged to: ');
+            for (var i = 0; i < appPath.length; i++) {
+                util.log(util.colors.cyan(appPath[i]));
+            }
+        }
+        done();
+    });
+});
+
+function runElectronApp() {
     return gulp.src("")
         .pipe(runElectron(["./dist/main.js"]));
 }
